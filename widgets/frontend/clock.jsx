a3bd17685782d44f78568312dd6da0e65b74c60e
apodavonov3616 <apodavonov3616@DESKTOP-BTS3T0E.localdomain>
2022-06-05 12:53:40 -0400

    created time

--- widgets/frontend/clock.jsx
+++ widgets/frontend/clock.jsx
@@ -2,12 +2,34 @@ import React from 'react';
 
 export default class Clock extends React.Component {
     constructor(props){
-        super(props)
+        super(props);
+        this.state = {
+            time: new Date()
+        };
+        this.tick = this.tick.bind(this)
+    }
+
+    tick() {
+        this.setState({time: new Date()})
     }
 
     render() {
+        let hours = this.state.time.getHours();
+        let minutes = this.state.time.getMinutes();
+        let seconds = this.state.seconds.getSeconds();
+
+        hours = (hours < 10) ? `0${hours}` : hours;
+        minutes = (minutes < 10) ? `0${minutes}` : minutes;
+        seconds = (seconds < 10) ? `0${seconds}` : seconds;
+
         return <div>
             <h1>My clock</h1>
+            <div>
+                <h2>Time:</h2>
+                <div>
+                    {hours}:{minutes}:{seconds}
+                </div>
+            </div>
         </div>
     }
 }
